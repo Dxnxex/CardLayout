247fe6388fe4f693d3f02b7ae6cdf5f2f037065d
Dxnxex <dxnxex@seznam.cz>
2025-01-05 18:25:01 +0100

    INIT COMMIT

--- src/main/java/cz/cardlayout/cardlayout/HelloApplication.java
+++ src/main/java/cz/cardlayout/cardlayout/HelloApplication.java
@@ -10,11 +10,19 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        String welcomeMsg = "Hello Java FX World!";;
+        System.out.println(welcomeMsg);
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle(welcomeMsg);
         stage.setScene(scene);
         stage.show();
+
+
+
+
     }
 
     public static void main(String[] args) {
